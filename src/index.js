6b95e95367a00f6a6541e4495633578d6fd025b7
Sino-Lut <slutya@gmail.com>
2025-05-27 01:07:08 +0200

    Added the AI API

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
-function generateProduct(event) {
-  event.preventDefault();
-
+function displayProduct(response) {
   new Typewriter("#product", {
-    strings: "Products about health and wellness",
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
   });
+}
+
+function generateProduct(event) {
+  event.preventDefault();
+
+  let instructionsInput = document.querySelector("#user-instructions");
+  let apiKey = "96t4883dea40738f8c4oe83b37b0ab90";
+  let context =
+    "You are an expert in describing anti-aging, health and wellness products. Please give me 6 lines in your description. I want 6 lines only ";
+  let prompt = `Generate product description about ${instructionsInput.value}`;
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+  axios.get(apiUrl).then(displayProduct);
 }
 
 let productFormElement = document.querySelector("#product-generator-form");
